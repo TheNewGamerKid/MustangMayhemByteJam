f26ce966cf4440829eb702defc2a8bd92144f414
FuzionToxic26 <115998318+FuzionToxic26@users.noreply.github.com>
2022-10-24 08:06:04 -0500

    updates made by ...

---
+++ Player/signshooting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class signshooting : MonoBehaviour
+{
+    public float signspeed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(LeftMouseClick))
+        {
+
+        }
+    }
+}
