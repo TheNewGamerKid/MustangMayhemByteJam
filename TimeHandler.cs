3b3d87dfef1f5b86eb06cc536c1fc2d07eb0b238
FireBoi9987 <James.drummond@dcmustangs.com>
2022-11-04 11:13:58 -0500

    Kole timer

---
+++ TimeHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeHandler : MonoBehaviour
+{
+
+    public float totalTime = 5f;
+    public float timeElapsed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeElapsed < totalTime) {
+            timeElapsed = Time.time;
+            GetComponent<TMPro.TextMeshPro>().text = Mathf.Ceil(totalTime - timeElapsed).ToString();
+        }
+    }
+}
